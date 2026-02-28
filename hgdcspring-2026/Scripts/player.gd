ab5699b31477c901491b66f49d5f6e5f99e38047
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-02-28 14:55:17 -0600

    projectiles shoot now

--- hgdcspring-2026/Scripts/player.gd
+++ hgdcspring-2026/Scripts/player.gd
@@ -13,14 +13,19 @@ var target:Vector2 = position
 var just_clicked = false
 var projectile_speed = 10
 
+#eyeball
+@onready var eyeball: Sprite2D = $Eyeball
+@onready var marker_2d: Marker2D = $Eyeball/Marker2D
+
+
 #reads inputs
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity = input_direction * speed
 func _input(event):
-	if event.is_action_pressed(&"click"):
+	target = get_global_mouse_position()
+	if event.is_action_pressed("Click"):
 		just_clicked = true
-		target = get_global_mouse_position()
 		create_projectile()
 
 
@@ -29,8 +34,15 @@ func create_projectile():
 	if just_clicked:
 		just_clicked = false
 		print("projectile")
-		#var projectile = 		
+		var projectile = projectile_scene.instantiate()
+		owner.add_child(projectile)
+		projectile.transform = marker_2d.global_transform
+	
+	
+func look_toward_mouse():
+	eyeball.look_at(target)
 #runs every tick
 func _physics_process(_delta):
 	get_input()
+	look_toward_mouse()
 	move_and_slide()
