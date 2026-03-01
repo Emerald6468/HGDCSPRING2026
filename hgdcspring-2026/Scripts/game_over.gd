26798a1c4def0e9d3a197585459b70f3aefc624a
RomanTheCoder05 <rdavis13@stedwards.edu>
2026-03-01 15:50:21 -0600

    restart button in game over

--- hgdcspring-2026/Scripts/game_over.gd
+++ hgdcspring-2026/Scripts/game_over.gd
@@ -26,4 +26,4 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 
 
 func _on_restart_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/TestScenes/ZanderTestScene.tscn")
