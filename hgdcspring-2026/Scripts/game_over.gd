e27a0990826682da05eb7e459d5f67a91eeb79f2
RomanTheCoder05 <rdavis13@stedwards.edu>
2026-03-01 15:36:25 -0600

    straight up implementing it, and by it haha, lets justr say, my UI

---
+++ hgdcspring-2026/Scripts/game_over.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+@onready var score: Label = $Overlay/ScoreLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$HighscoreMenuButtons.visible = false
+	$GameOverMenuButtons.visible = false
+	$Restart.visible = false
+	$Overlay/ScoreLabel.visible = false
+	$Overlay/AnimationPlayer.play("fade_to_screen")
+	var high_score:int = Global.save_data.high_score
+	score.text = str(high_score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	$HighscoreMenuButtons.visible = true
+	$GameOverMenuButtons.visible = true
+	$Restart.visible = true
+	$Overlay/ScoreLabel.visible = true
+
+
+func _on_restart_pressed() -> void:
+	pass # Replace with function body.
